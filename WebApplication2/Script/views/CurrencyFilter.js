94d72416789cb0441b2c7a1238fa1a4c7883d733
Nivash <nivash@outlook.com>
2017-05-16 00:58:31 +0530

    delete functionality

---
+++ WebApplication2/Script/views/CurrencyFilter.js
@@ -0,0 +1,21 @@
+﻿
+var app = angular.module("app1", []);
+
+app.filter("indcurr", function () {
+    return function (input) {
+        input = input + "";
+        if (input.length === 4) {
+            input = input.substring(0, 1) + "," + input.substring(1, 4);
+        }
+        else if (input.length === 5) {
+            input = input.substring(0,2) + "," + input.substring(2, 5);
+        }
+        else if (input.length === 6) {
+            input = input.substring(0, 1) + "," + input.substring(1, 2) + "," + input.substring(3, 6);
+        }
+        else if (input.length === 7) {
+            input = input.substring(0, 2) + "," + input.substring(2, 4) + "," + input.substring(4, 7);
+        }
+        return "RS. " + input;
+    };
+});
--- WebApplication2/Script/views/Index.js
+++ WebApplication2/Script/views/Index.js
@@ -1,65 +1,83 @@
 ﻿
-var app = angular.module("app1", [])
+var app = angular.module("app1");
 
-app.controller("c1", function ($scope) {
-    $scope.currentDate = new Date();
-    $scope.salaryMonthYear = "";
-    $scope.salaryamt = "";
-    $scope.SalaryList = [];
-    $scope.SalaryListLocal = [];
-    $scope.TotalSalaryIncome = 0;
+app.controller("c1",
+    function($scope) {
 
-    $scope.addSalary = function () {
-        if ($scope.salaryMonthYear != "") {
-            var my = $scope.salaryMonthYear.split("/");
-            var nDate = new Date(my[1], parseInt(my[0])-1, 2);
-            $scope.SalaryListLocal.push({ dt: nDate, am: $scope.salaryamt });         
-            calc();
-            $scope.salaryMonthYear = "";
-            $scope.salaryamt = "";
-        }
-    }
-
-    var calc = function () {
-        var cDate = $scope.SalaryListLocal[0].dt;
-        cDate = new Date(cDate.getFullYear(), cDate.getMonth(), cDate.getDate())
+        $scope.currentDate = new Date();
+        $scope.salaryMonthYear = "";
+        $scope.salaryamt = "";
         $scope.SalaryList = [];
+        $scope.SalaryListLocal = [];
         $scope.TotalSalaryIncome = 0;
-        for ( cDate ; cDate < $scope.currentDate; cDate.setMonth(cDate.getMonth() + 1)) {
-            var thisamt = getSalary(cDate);
-            $scope.TotalSalaryIncome += parseFloat(thisamt);
-            $scope.SalaryList.push({ dt: new Date(cDate.getFullYear(), cDate.getMonth(), cDate.getDate()), am: thisamt });
+
+       
+
+        var getSalary = function(adate) {
+            var thisamt = 0;
+            for (var i = 0; i < $scope.SalaryListLocal.length; i++) {
+                var ad = $scope.SalaryListLocal[i].dt;
+                if (ad <= adate)
+                    thisamt = $scope.SalaryListLocal[i].am;
+            }
+            return parseFloat(thisamt);
+        };
+        var calc = function () {
+            $scope.SalaryList = [];
+            $scope.TotalSalaryIncome = 0;
+            if ($scope.SalaryListLocal.length > 0) {
+                var cDate = $scope.SalaryListLocal[0].dt;
+                cDate = new Date(cDate.getFullYear(), cDate.getMonth(), cDate.getDate());
+                for (cDate; cDate < $scope.currentDate; cDate.setMonth(cDate.getMonth() + 1)) {
+                    var thisamt = getSalary(cDate);
+                    $scope.TotalSalaryIncome += parseFloat(thisamt);
+                    $scope.SalaryList.push({
+                        dt: new Date(cDate.getFullYear(), cDate.getMonth(), cDate.getDate()),
+                        am: thisamt
+                    });
+                }
+            } 
+        };
+        $scope.addSalary = function() {
+            if ($scope.salaryMonthYear !== "") {
+                var my = $scope.salaryMonthYear.split("/");
+                var nDate = new Date(my[1], parseInt(my[0]) - 1, 2);
+                $scope.SalaryListLocal.push({ dt: nDate, am: $scope.salaryamt });
+                calc();
+                $scope.salaryMonthYear = "";
+                $scope.salaryamt = "";
+            }
+        };
+
+        $scope.delSalary = function(salary) {
+            var index = $scope.SalaryListLocal.indexOf(salary);
+            $scope.SalaryListLocal.splice(index, 1);
+            calc();
+        };
+    });
+
+app.directive("salary",
+    function() {
+        return {
+            controller: "c1",
+            template: angular.element("#idsalaryformview").html(),
+            replace: false
+        };
+    });
+
+app.directive("salaryList",
+    [
+        function() {
+            return {
+                scope: {
+                    ShowDelete: "=shdel",
+                    SalaryListRemote: "=info",
+                    delete1: "&onDelete"
+                    
+                },
+                controller: "c1",
+                template: angular.element("#idofthedive").html(),
+                replace: false
+            };
         }
-    };
-
-    var getSalary = function(adate){
-        var thisamt = 0;
-        for (var i = 0; i < $scope.SalaryListLocal.length; i++) {
-            var ad = $scope.SalaryListLocal[i].dt;
-            if (ad <= adate)
-                thisamt = $scope.SalaryListLocal[i].am;
-        }
-        return parseFloat(thisamt);
-    }
-
-
-        
-});
-
-app.directive("salary", function () {
-    return {
-        controller: "c1",
-        templateUrl: "Home/SalaryForm",
-        replace: false
-    };
-});
-
-
-app.directive("salaryList", function () {
-    return {
-        scope: { SalaryListRemote: "=info"},
-        controller: "c1",
-        templateUrl: "Home/SalaryListView",
-        replace: false
-    };
-});
+    ]);
